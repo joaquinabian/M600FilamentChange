3cda3cb06da59b2a4566d873622fc71f6e9e883d
Jeff Epler <jepler@unpythonic.net>
2020-10-25 08:10:05 -0500

    initial hacking

--- octoprint_RewriteM600/__init__.py
+++ octoprint_RewriteM600/__init__.py
@@ -11,64 +11,93 @@ from __future__ import absolute_import
 
 import octoprint.plugin
 
-class Rewritem600Plugin(octoprint.plugin.AssetPlugin, octoprint.plugin.TemplatePlugin, octoprint.plugin.SettingsPlugin):
-	def rewrite_m600(self, comm_instance, phase, cmd, cmd_type, gcode, *args, **kwargs):
-		if gcode and gcode == "M600":
-			self._plugin_manager.send_plugin_message(self._identifier, dict(type="popup", msg="Please change the filament and resume the print"))
-			comm_instance.setPause(True)
-			cmd = [("M117 Filament Change",),"G91","M83", "G1 Z+"+str(self._settings.get(["zDistance"]))+" E-0.8 F4500", "M82", "G90", "G1 X0 Y0"]
-		return cmd
 
-	def after_resume(self, comm_instance, phase, cmd, parameters, tags=None, *args, **kwargs):
-		if cmd and cmd == "resume":
-			if(comm_instance.pause_position.x):
-				cmd = []
-				cmd =["M83","G1 E-0.8 F4500", "G1 E0.8 F4500", "G1 E0.8 F4500", "M82", "G90", "G92 E"+str(comm_instance.pause_position.e), "M83", "G1 X"+str(comm_instance.pause_position.x)+" Y"+str(comm_instance.pause_position.y)+" Z"+str(comm_instance.pause_position.z)+" F4500"]
-				if(comm_instance.pause_position.f):
-					cmd.append("G1 F" + str(comm_instance.pause_position.f))
-				comm_instance.commands(cmd)
-			comm_instance.setPause(False)
-		return
-	def get_settings_defaults(self):
-		return dict(zDistance=80)
+class Rewritem600Plugin(
+    octoprint.plugin.AssetPlugin,
+    octoprint.plugin.TemplatePlugin,
+    octoprint.plugin.SettingsPlugin,
+):
+    def rewrite_m600(self, comm_instance, phase, cmd, cmd_type, gcode, *args, **kwargs):
+        if gcode and gcode == "M600":
+            self._plugin_manager.send_plugin_message(
+                self._identifier,
+                dict(
+                    type="popup", msg="Please change the filament and resume the print"
+                ),
+            )
+            comm_instance.setPause(True)
+            cmd = [
+                ("M117 Filament Change",),
+                "G91",  # relative positioning
+                "G1 Z" + str(self._settings.get(["zDistance"])),
+                "G90",  # absolute position
+                "G1 X" + str(self._settings.get(["toolChangeX"]) + " Y" + str(self._settings.get(["toolChangeY"]), # go to filament change location
+            ]
+        return cmd
 
-	def get_template_configs(self):
-		return [
-			dict(type="navbar", custom_bindings=False),
-			dict(type="settings", custom_bindings=False)
-		]
-	##~~ AssetPlugin mixin
+    def after_resume(
+        self, comm_instance, phase, cmd, parameters, tags=None, *args, **kwargs
+    ):
+        if cmd and cmd == "resume":
+            if comm_instance.pause_position.x:
+                cmd = []
+                cmd = [
+                    "G91",  # relative positioning
+                    "G1 Z" + str(self._settings.get(["zDistance"])),
+                    "G90",  # Absolute Positioning
+                    "G1 X"
+                    + str(comm_instance.pause_position.x)
+                    + " Y"
+                    + str(comm_instance.pause_position.y)
+                    + " Z"
+                    + str(comm_instance.pause_position.z)
+                    + " F4500",
+                ]
+                if comm_instance.pause_position.f:
+                    cmd.append("G1 F" + str(comm_instance.pause_position.f))
+                comm_instance.commands(cmd)
+            comm_instance.setPause(False)
+        return
 
-	def get_assets(self):
-		# Define your plugin's asset files to automatically include in the
-		# core UI here.
-		return dict(
-			js=["js/RewriteM600.js"],
-			css=["css/RewriteM600.css"],
-			less=["less/RewriteM600.less"]
-		)
+    def get_settings_defaults(self):
+        return dict(zDistance=5, toolChangeX=151.9, toolChangeY=0)
 
-	##~~ Softwareupdate hook
+    def get_template_configs(self):
+        return [
+            dict(type="navbar", custom_bindings=False),
+            dict(type="settings", custom_bindings=False),
+        ]
 
-	def get_update_information(self):
-		# Define the configuration for your plugin to use with the Software Update
-		# Plugin here. See https://docs.octoprint.org/en/master/bundledplugins/softwareupdate.html
-		# for details.
-		return dict(
-			RewriteM600=dict(
-				displayName="Rewritem600 Plugin",
-				displayVersion=self._plugin_version,
+    ##~~ AssetPlugin mixin
 
-				# version check: github repository
-				type="github_release",
-				user="wgcv",
-				repo="RewriteM600",
-				current=self._plugin_version,
+    def get_assets(self):
+        # Define your plugin's asset files to automatically include in the
+        # core UI here.
+        return dict(
+            js=["js/RewriteM600.js"],
+            css=["css/RewriteM600.css"],
+            less=["less/RewriteM600.less"],
+        )
 
-				# update method: pip
-				pip="https://github.com/wgcv/RewriteM600/archive/{target_version}.zip"
-			)
-		)
+    ##~~ Softwareupdate hook
+
+    def get_update_information(self):
+        # Define the configuration for your plugin to use with the Software Update
+        # Plugin here. See https://docs.octoprint.org/en/master/bundledplugins/softwareupdate.html
+        # for details.
+        return dict(
+            RewriteM600=dict(
+                displayName="Rewritem600 Plugin",
+                displayVersion=self._plugin_version,
+                # version check: github repository
+                type="github_release",
+                user="jepler",
+                repo="RewriteM600",
+                current=self._plugin_version,
+                # update method: pip
+                pip="https://github.com/jepler/RewriteM600/archive/{target_version}.zip",
+            )
+        )
 
 
 # If you want your plugin to be registered within OctoPrint under a different name than what you defined in setup.py
@@ -79,18 +108,18 @@ __plugin_name__ = "Filament Change - M600 Rewriter"
 # Starting with OctoPrint 1.4.0 OctoPrint will also support to run under Python 3 in addition to the deprecated
 # Python 2. New plugins should make sure to run under both versions for now. Uncomment one of the following
 # compatibility flags according to what Python versions your plugin supports!
-#__plugin_pythoncompat__ = ">=2.7,<3" # only python 2
-#__plugin_pythoncompat__ = ">=3,<4" # only python 3
-__plugin_pythoncompat__ = ">=2.7,<4" # python 2 and 3
+# __plugin_pythoncompat__ = ">=2.7,<3" # only python 2
+# __plugin_pythoncompat__ = ">=3,<4" # only python 3
+__plugin_pythoncompat__ = ">=2.7,<4"  # python 2 and 3
+
 
 def __plugin_load__():
-	global __plugin_implementation__
-	__plugin_implementation__ = Rewritem600Plugin()
+    global __plugin_implementation__
+    __plugin_implementation__ = Rewritem600Plugin()
 
-	global __plugin_hooks__
-	__plugin_hooks__ = {
-		"octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information,
-		"octoprint.comm.protocol.gcode.queuing": __plugin_implementation__.rewrite_m600,
-		"octoprint.comm.protocol.atcommand.queuing": __plugin_implementation__.after_resume,
-	}
-
+    global __plugin_hooks__
+    __plugin_hooks__ = {
+        "octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information,
+        "octoprint.comm.protocol.gcode.queuing": __plugin_implementation__.rewrite_m600,
+        "octoprint.comm.protocol.atcommand.queuing": __plugin_implementation__.after_resume,
+    }
